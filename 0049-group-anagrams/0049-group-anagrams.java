27d8fda7f6ef7c2040f3cd5f449c6e5d6507ee5c
Akshay Khare <61112118+akshaykhare19@users.noreply.github.com>
2026-08-01 22:06:17 +0530

    Time: 8 ms (34.65%), Space: 49.6 MB (58.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -6,11 +6,15 @@ class Solution {
             char charArr[] = str.toCharArray();
             Arrays.sort(charArr);
             String sortedStr = new String(charArr);
-            if(!map.containsKey(sortedStr))
+            
+            map.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(str);
+            //can be replaced with above line completely.
+            /*if(!map.containsKey(sortedStr))
                 map.put(sortedStr, new ArrayList<>());
             List<String> tempList = map.get(sortedStr);
-            tempList.add(str);
-            map.put(sortedStr, tempList);
+            tempList.add(str);*/
+
+            // map.put(sortedStr, tempList); //not needed, since tempList has the same instance. Hence, this can be removed.
         }
         map.forEach((key, value) -> res.add(value));
         return res;
