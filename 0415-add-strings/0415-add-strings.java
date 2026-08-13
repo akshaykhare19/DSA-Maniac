f00063c8b5c046294ecb4a5bfa9e44ed68ceb020
Akshay Khare <61112118+akshaykhare19@users.noreply.github.com>
2026-08-13 17:05:24 +0530

    Time: 3 ms (24.83%), Space: 45.1 MB (17.3%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder("");
+        int n = num1.length(), m = num2.length();
+        int i = n-1, j = m-1, carry = 0, value = 0;
+        
+        while(i >= 0 && j >= 0) {
+            int n1 = Character.getNumericValue(num1.charAt(i));
+            int n2 = Character.getNumericValue(num2.charAt(j));
+
+            int digSum = n1+n2+carry;
+            carry = digSum/10;
+            value = digSum%10;
+
+            sb.append(value+"");
+            i--;
+            j--;
+        }
+
+        while(i>=0) {
+            int n1 = Character.getNumericValue(num1.charAt(i));
+            int digSum = n1 + carry;
+            carry = digSum/10;
+            value = digSum%10;
+            sb.append(value+"");
+            i--;
+        }
+
+        while(j>=0) {
+            int n2 = Character.getNumericValue(num2.charAt(j));
+            int digSum = n2 + carry;
+            carry = digSum/10;
+            value = digSum%10;
+            sb.append(value+"");
+            j--;
+        }
+
+        if(carry>0) sb.append(carry+"");
+        // sb.reverse();
+        return sb.reverse().toString();
+
+    }
+}
